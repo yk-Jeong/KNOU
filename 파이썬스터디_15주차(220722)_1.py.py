fcd85b2a4da38d6de675a13ec6389a083938f4fd
Jeong YK/Ellie <90163856+yk-Jeong@users.noreply.github.com>
2022-07-23 14:19:32 +0900

    Add files via upload

---
+++ 파이썬스터디_15주차(220722)_1.py.py
@@ -0,0 +1,10 @@
+import turtle as t
+for i in range(20):
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(100)
+    t.right(72)
